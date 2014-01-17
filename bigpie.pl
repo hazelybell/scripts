6a60b093b910aa1dab3a2f1a5e38b281794a19e1
Joshua Charles Campbell <gitcommit@orezpraw.com>
2014-01-17 12:59:21 -0700

    chomps

--- bigpie.pl
+++ bigpie.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+# Use like: bigpie.pl <file containing search string> <file containing replace string> <files to search and replace in>
+
 # based on code by almut on Apr 08, 2010 at 15:32 UTC
 # http://www.perlmonks.org/?node_id=833495, Jan 17, 2014
 
@@ -20,7 +22,9 @@ sub inplace_edit {
 }
 
 my $f = read_file(shift @ARGV);
+chomp $f;
 my $r = read_file(shift @ARGV);
+chomp $r;
 
 inplace_edit(
     sub {
