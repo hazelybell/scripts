269b07667125b08a5a0ecac81c5d98522b3c7dc4
Joshua Charles Campbell <gitcommit@orezpraw.com>
2018-09-20 12:36:52 -0600

    change dpi

--- 720x2.sh
+++ 720x2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 xrandr \
-	--output eDP-1-1 --primary --mode 1280x720 --dpi 112 \
-	--output HDMI-1 --mode 1280x720 --dpi 112 --same-as eDP-1-1 \
-	--output DP-1 --mode 1280x720 --dpi 112 --above eDP-1-1
+	--output eDP-1-1 --primary --mode 1280x720 --dpi 120 \
+	--output HDMI-1 --mode 1280x720 --dpi 120 --same-as eDP-1-1 \
+	--output DP-1 --mode 1280x720 --dpi 120 --above eDP-1-1
