f7ca50c4c59c88ebe318c47906a8caf359c04070
Joshua Charles Campbell <gitcommit@orezpraw.com>
2018-09-20 12:28:31 -0600

    add xrandr script

---
+++ 720x2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xrandr \
+	--output eDP-1-1 --primary --mode 1280x720 --dpi 112 \
+	--output HDMI-1 --mode 1280x720 --dpi 112 --same-as eDP-1-1 \
+	--output DP-1 --mode 1280x720 --dpi 112 --above eDP-1-1
