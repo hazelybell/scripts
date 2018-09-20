2ed96d47ea7836098939393a72e459167cccd780
Joshua Charles Campbell <gitcommit@orezpraw.com>
2018-09-20 14:26:37 -0600

    setup script

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ ! -e ~/scripts/setup.sh ]]
+then	echo "Expected scripts to be in ~/scripts"
+	exit 1
+fi
+
+cd "$HOME"
+function lns() {
+	ln --backup=numbered --suffix=.old \
+		--verbose --symbolic "$@"
+}
+
+function replace() {
+	if [[ -z "$2" ]]
+	then	source="scripts/$1"
+		destination="$1"
+	else	source="scripts/$1"
+		destination="$2"
+	fi
+	if [[ ! -e $source ]]
+	then echo "$source is missing!"
+	fi
+	if [[ -e "$destination" ]]
+	then if [[ -h "$destination" ]]
+		then rm -vf "$destination"
+		else lns "$source" "$destination"
+		fi
+	fi
+}
+
+replace .inputrc
+replace .toprc
+replace .screenrc
+replace .bashrc
+replace konsolerc .config/konsolerc
+replace konsole .local/share/konsole
